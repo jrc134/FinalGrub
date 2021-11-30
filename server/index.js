9006e46bec4f5fe97098be8dc7f15ff8bda7dd00
jrc134 <jrc134@humboldt.edu>
2021-11-29 18:15:03 -0800

    No URL

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://newuser01:Password1234@cluster0.yuwij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
